6ce6a42611adae8e99563c7259887e9fc5854ef5
Michael Kendall <mkendall@cradlepoint.com>
2020-04-22 10:16:07 -0600

    Updated .js to run before closing of body

--- lesson2/source.js
+++ lesson2/source.js
@@ -1,4 +1,2 @@
-function updated() {
-    document.getElementById("copyright").innerHTML = new Date().getFullYear();
-    document.getElementById("modified").innerHTML = "Last updated: " + document.lastModified;
-    }
+document.getElementById("copyright").innerHTML = new Date().getFullYear();
+document.getElementById("modified").innerHTML = "Last updated: " + document.lastModified;
