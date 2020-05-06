9143b9af4c64fa2345324d96730b5f3794baf187
techmike12 <mkendall32@gmail.com>
2020-05-06 10:00:12 -0600

    Updated

--- lesson4/js/source.js
+++ lesson4/js/source.js
@@ -1,2 +1,2 @@
 document.getElementById("copyright").innerHTML = new Date().getFullYear();
-document.getElementById("modified").innerHTML = "Last updated: " + document.lastModified;
+document.getElementById("modified").innerHTML = document.lastModified;
