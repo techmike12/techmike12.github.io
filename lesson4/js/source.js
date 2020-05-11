f24b657b720dfdd6a964d648f514bff01f62cbe3
techmike12 <mkendall32@gmail.com>
2020-05-11 11:40:21 -0600

    Updated Footer

--- lesson4/js/source.js
+++ lesson4/js/source.js
@@ -1,2 +1,74 @@
-document.getElementById("copyright").innerHTML = new Date().getFullYear();
-document.getElementById("modified").innerHTML = document.lastModified;
+document.getElementById("modified").innerHTML = todayDate();
+
+function todayDate() {
+    var today = new Date();
+    var month = today.getMonth() + 1;
+    switch (month) {
+        case 0:
+            month = "January"
+            break;
+        case 1:
+            month = "February"
+            break;
+        case 2:
+            month = "March"
+            break;
+        case 3:
+            month = "April"
+            break;
+        case 4:
+            month = "May"
+            break;
+        case 5:
+            month = "June"
+            break;
+        case 6:
+            month = "July"
+            break;
+        case 7:
+            month = "August"
+            break;
+        case 8:
+            month = "September"
+            break;
+        case 9:
+            month = "October"
+            break;
+        case 10:
+            month = "November"
+            break;
+        case 11:
+            month = "December"
+            break;
+        case 12:
+            month = "Saturday"
+            break;
+        }
+    var day = today.getDay();
+    switch (day) {
+        case 1:
+            day = "Monday"
+            break;
+        case 2:
+            day = "Tuesday"
+            break;
+        case 3:
+            day = "Wednesday"
+            break;
+        case 4:
+            day = "Thursday"
+            break;
+        case 5:
+            day = "Friday"
+            break;
+        case 6:
+            day = "Saturday"
+            break;
+        case 0:
+            day = "Sunday"
+            break;
+    }
+    var dayDate = today.getDate();
+    var year = today.getFullYear();
+    return  day + ", " + dayDate + " " + month + " " + year;
+}
