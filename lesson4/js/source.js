8c010fe83ac50836a0eae81e303b2ed18be4c948
techmike12 <mkendall32@gmail.com>
2020-05-11 19:07:14 -0600

    Updated

--- lesson4/js/source.js
+++ lesson4/js/source.js
@@ -2,72 +2,10 @@ document.getElementById("modified").innerHTML = todayDate();
 
 function todayDate() {
     var today = new Date();
-    var month = today.getMonth();
-    switch (month) {
-        case 0:
-            month = "January"
-            break;
-        case 1:
-            month = "February"
-            break;
-        case 2:
-            month = "March"
-            break;
-        case 3:
-            month = "April"
-            break;
-        case 4:
-            month = "May"
-            break;
-        case 5:
-            month = "June"
-            break;
-        case 6:
-            month = "July"
-            break;
-        case 7:
-            month = "August"
-            break;
-        case 8:
-            month = "September"
-            break;
-        case 9:
-            month = "October"
-            break;
-        case 10:
-            month = "November"
-            break;
-        case 11:
-            month = "December"
-            break;
-        case 12:
-            month = "Saturday"
-            break;
-        }
-    var day = today.getDay();
-    switch (day) {
-        case 1:
-            day = "Monday"
-            break;
-        case 2:
-            day = "Tuesday"
-            break;
-        case 3:
-            day = "Wednesday"
-            break;
-        case 4:
-            day = "Thursday"
-            break;
-        case 5:
-            day = "Friday"
-            break;
-        case 6:
-            day = "Saturday"
-            break;
-        case 0:
-            day = "Sunday"
-            break;
-    }
+    const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+    const months = ["January", "February", "March", "April", "May", "June","July", "August", "September", "October", "November", "December"];
+    var month = months[today.getMonth()];
+    var day = days[today.getDay()];
     var dayDate = today.getDate();
     var year = today.getFullYear();
     return  day + ", " + dayDate + " " + month + " " + year;
