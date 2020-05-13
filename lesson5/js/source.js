cba50219a6d89c99468c71553505b1d07dd5142b
techmike12 <mkendall32@gmail.com>
2020-05-13 09:59:16 -0600

    Updated

--- lesson5/js/source.js
+++ lesson5/js/source.js
@@ -1,8 +1,8 @@
+/*Display date in the footer*/
 document.getElementById("modified").innerHTML = todayDate();
-
 function todayDate() {
     var today = new Date();
-    const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+    const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
     const months = ["January", "February", "March", "April", "May", "June","July", "August", "September", "October", "November", "December"];
     var month = months[today.getMonth()];
     var day = days[today.getDay()];
@@ -11,6 +11,7 @@ function todayDate() {
     return  day + ", " + dayDate + " " + month + " " + year;
 }
 
+/*Hamburger menu for small display*/
 function toggleMenu ()  {
     if (document.getElementsByClassName("navigation")[0].classList == "navigation"){
         document.getElementsByClassName("navigation")[0].classList.toggle("responsive");
@@ -20,4 +21,18 @@ function toggleMenu ()  {
         document.getElementById("ham").innerHTML= "&#9776 Menu";
     }
 
+}
+
+/*Banner at top of screen for Friday's*/
+document.getElementById("banner").innerHTML = banner();
+function banner() {
+    var today = new Date();
+    const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+    var day = days[today.getDay()];
+    if (day == "Friday") {
+        document.getElementById("banner").style.display="flex";
+    } else {
+        document.getElementById("banner").style.display="none";
+    }
+    return "Saturday = Preston Pancakes in the Park!  9:00 a.m. Saturday at the city park pavilion."
 }
