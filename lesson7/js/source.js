e70bd90a7bbc635b080ef94631a2d4e7a94a9378
techmike12 <mkendall32@gmail.com>
2020-05-28 10:50:46 -0600

    Updated

--- lesson7/js/source.js
+++ lesson7/js/source.js
@@ -41,7 +41,7 @@ function banner() {
 const images = document.querySelectorAll("img[data-src]");
 
 const imgOptions = {
-    threshold: 0,
+    threshold: 1,
     rootMargin: "0px 0px 50px 0px"
 };
 
