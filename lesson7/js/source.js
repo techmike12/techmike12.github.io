b5cb4baa857416d1bd27faf6416df6413b0a09cf
techmike12 <mkendall32@gmail.com>
2020-06-01 10:24:37 -0600

    Updated

--- lesson7/js/source.js
+++ lesson7/js/source.js
@@ -23,20 +23,6 @@ function toggleMenu ()  {
 
 }
 
-/*Banner at top of screen for Friday's*/
-document.getElementById("banner").innerHTML = banner();
-function banner() {
-    var today = new Date();
-    const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-    var day = days[today.getDay()];
-    if (day == "Friday") {
-        document.getElementById("banner").style.display="flex";
-    } else {
-        document.getElementById("banner").style.display="none";
-    }
-    return "Saturday = Preston Pancakes in the Park!  9:00 a.m. Saturday at the city park pavilion."
-}
-
 /*Load images as user scrolls*/
 const images = document.querySelectorAll("img[data-src]");
 
