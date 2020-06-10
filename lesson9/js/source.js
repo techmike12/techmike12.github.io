b79770f0b20bb649f7bbe6ddcd65355346571e6b
techmike12 <mkendall32@gmail.com>
2020-06-10 09:57:13 -0600

    Updated

--- lesson9/js/source.js
+++ lesson9/js/source.js
@@ -27,3 +27,38 @@ function toggleMenu ()  {
 function adjustRating(rating) {
     document.getElementById("ratingvalue").innerHTML = rating;
 }
+
+/*Getting data for towns*/
+const requestURL = 'https://techmike12/techmike12.github.io\lesson9\data\towndata.json';
+
+fetch(requestURL)
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (jsonObject) {
+    console.table(jsonObject);  // temporary checking for valid response and data parsing
+    const towns = jsonObject['towns'];
+
+    //Create elements and content in HTML
+    for (let i = 0; i < towns.length; i++ ) {
+        let card = document.createElement('section');
+        let h2 = document.createElement('h2');
+        let birth = document.createElement('p');
+        let birthplace = document.createElement('p');
+        let image = document.createElement('img');
+
+        //Create content
+        birth.textContent = 'Date of Birth: ' + prophets[i].birthdate;
+        birthplace.textContent = 'Place of Birth: ' + prophets[i].birthplace;
+        image.setAttribute('src', prophets[i].imageurl);
+        image.setAttribute('alt', prophets[i].name + prophets.lastname + " - " + prophets.order);
+        h2.textContent = prophets[i].name + ' ' + prophets[i].lastname;
+
+        //Add elements into section
+        card.appendChild(h2);
+        card.appendChild(birth);
+        card.appendChild(birthplace);
+        card.appendChild(image);
+        document.querySelector('div.towncard').appendChild(card);
+    }
+  });
