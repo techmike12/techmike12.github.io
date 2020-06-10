ec5af74aeb800c37e42ec4c38594f1367150ff47
techmike12 <mkendall32@gmail.com>
2020-06-10 09:03:44 -0600

    Lesson9

---
+++ lesson9/js/source.js
@@ -0,0 +1,29 @@
+/*Display date in the footer*/
+document.getElementById("modified").innerHTML = todayDate();
+function todayDate() {
+    var today = new Date();
+    const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+    const months = ["January", "February", "March", "April", "May", "June","July", "August", "September", "October", "November", "December"];
+    var month = months[today.getMonth()];
+    var day = days[today.getDay()];
+    var dayDate = today.getDate();
+    var year = today.getFullYear();
+    return  day + ", " + dayDate + " " + month + " " + year;
+}
+
+/*Hamburger menu for small display*/
+function toggleMenu ()  {
+    if (document.getElementsByClassName("navigation")[0].classList == "navigation"){
+        document.getElementsByClassName("navigation")[0].classList.toggle("responsive");
+        document.getElementById("ham").innerHTML= "&#x2715";
+    } else {
+        document.getElementsByClassName("navigation")[0].classList.toggle("responsive");
+        document.getElementById("ham").innerHTML= "&#9776 Menu";
+    }
+
+}
+
+/*Rating*/
+function adjustRating(rating) {
+    document.getElementById("ratingvalue").innerHTML = rating;
+}
---
+++ lesson9/prophets/js/prophets.js
@@ -0,0 +1,33 @@
+const requestURL = 'https://byui-cit230.github.io/lessons/lesson-09/data/latter-day-prophets.json';
+
+fetch(requestURL)
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (jsonObject) {
+    console.table(jsonObject);  // temporary checking for valid response and data parsing
+    const prophets = jsonObject['prophets'];
+
+    //Create elements and content in HTML
+    for (let i = 0; i < prophets.length; i++ ) {
+        let card = document.createElement('section');
+        let h2 = document.createElement('h2');
+        let birth = document.createElement('p');
+        let birthplace = document.createElement('p');
+        let image = document.createElement('img');
+
+        //Create content
+        birth.textContent = 'Date of Birth: ' + prophets[i].birthdate;
+        birthplace.textContent = 'Place of Birth: ' + prophets[i].birthplace;
+        image.setAttribute('src', prophets[i].imageurl);
+        image.setAttribute('alt', prophets[i].name + prophets.lastname + " - " + prophets.order);
+        h2.textContent = prophets[i].name + ' ' + prophets[i].lastname;
+
+        //Add elements into section
+        card.appendChild(h2);
+        card.appendChild(birth);
+        card.appendChild(birthplace);
+        card.appendChild(image);
+        document.querySelector('div.cards').appendChild(card);
+    }
+  });
