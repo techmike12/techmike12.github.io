232c1863642bc79ac851702d6756796df495ad41
techmike12 <mkendall32@gmail.com>
2020-06-15 08:28:59 -0600

    Updated

--- lesson9/js/source.js
+++ lesson9/js/source.js
@@ -41,28 +41,33 @@ fetch(requestURL)
             //Create Element Variables
             let card = document.createElement('section');
             let image = document.createElement('img');
-            let name = document.createElement('h2');
-            let motto = document.createElement('h4');
-            let year = document.createElement('p');
-            let pop = document.createElement('p');
-            let rain = document.createElement('p');
-
-            //Create Content
             image.setAttribute('src', 'Images/' + towns[i].photo);
             image.setAttribute('alt', towns[i].name);
+            image.setAttribute('class', 'cityImage');
+            let prefix = towns[i].name == "Preston" ? "p" : towns[i].name == "Soda Springs" ? "ss" : towns[i].name == "Fish Haven" ? "fh" : "";
+            image.setAttribute('id', prefix + '_image');
+            let detail = document.createElement('div');
+            detail.setAttribute('class', 'cityDetail')
+            detail.setAttribute('id', prefix + '_detail');
+            let name = document.createElement('h2');
             name.textContent = towns[i].name;
+            let motto = document.createElement('h4');
             motto.textContent = '\"' + towns[i].motto +'\"';
+            let year = document.createElement('p');
             year.textContent = 'Year Founded: ' + towns[i].yearFounded;
+            let pop = document.createElement('p');
             pop.textContent = 'Population: ' + towns[i].currentPopulation;
+            let rain = document.createElement('p');
             rain.textContent = 'Annual Rain Fall: ' + towns[i].averageRainfall;
 
             //Add elements into section
             card.appendChild(image);
-            card.appendChild(name);
-            card.appendChild(motto);
-            card.appendChild(year);
-            card.appendChild(pop);
-            card.appendChild(rain);
+            card.appendChild(detail);
+            detail.appendChild(name);
+            detail.appendChild(motto);
+            detail.appendChild(year);
+            detail.appendChild(pop);
+            detail.appendChild(rain);
             document.querySelector('div.towncard').appendChild(card);
         }
     }
