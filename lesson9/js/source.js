102276e16fe22a189f98a7a0b8ccb09ed26ce7bc
techmike12 <mkendall32@gmail.com>
2020-06-15 18:40:52 -0600

    Updated

--- lesson9/js/source.js
+++ lesson9/js/source.js
@@ -92,3 +92,44 @@ function banner() {
     }
     return "Saturday = Preston Pancakes in the Park!  9:00 a.m. Saturday at the city park pavilion."
 }
+
+/*Rating*/
+function adjustRating(rating) {
+    document.getElementById("ratingvalue").innerHTML = rating;
+}
+
+/*Load images as user scrolls*/
+const images = document.querySelectorAll("img[data-src]");
+
+const imgOptions = {
+    threshold: 0,
+    rootMargin: "0px 0px 50px 0px"
+};
+
+const loadImages = (img) => {
+    img.setAttribute('src', img.getAttribute('data-src'));
+    img.onload = () => {
+      img.removeAttribute('data-src');
+    };
+}
+
+if("IntersectionObserver" in window) {
+const imgObserver = new IntersectionObserver((entries, imgObserver) => {
+    entries.forEach((entries) => {
+        if (!entries.isIntersecting) {
+            return;
+        } else {
+            loadImages(entries.target);
+            imgObserver.unobserve(entries.target);
+        }
+    });
+}, imgOptions);
+
+images.forEach(image => {
+    imgObserver.observe(image);
+});
+} else {
+    imagesToLoad.forEach((img) => {
+        loadImages(img);
+    });
+}
