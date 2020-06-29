7639e190903d5bf9ecb7c34920e83f7e6a7387ed
techmike12 <mkendall32@gmail.com>
2020-06-29 11:43:54 -0600

    Updated Footer

--- Scoots/js/source.js
+++ Scoots/js/source.js
@@ -1,15 +1,5 @@
 /*Display date in the footer*/
-document.getElementById("modified").innerHTML = todayDate();
-function todayDate() {
-    var today = new Date();
-    const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-    const months = ["January", "February", "March", "April", "May", "June","July", "August", "September", "October", "November", "December"];
-    var month = months[today.getMonth()];
-    var day = days[today.getDay()];
-    var dayDate = today.getDate();
-    var year = today.getFullYear();
-    return  day + ", " + dayDate + " " + month + " " + year;
-}
+document.getElementById("modified").innerHTML = "Last updated: " + document.lastModified;
 
 /*Hamburger menu for small display*/
 function toggleMenu ()  {
