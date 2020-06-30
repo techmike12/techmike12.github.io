fd05c3c3c4c6461c27c8e39237a8dee675b76eef
techmike12 <mkendall32@gmail.com>
2020-06-30 13:50:09 -0600

    Updated JSON

--- Scoots/js/source.js
+++ Scoots/js/source.js
@@ -1,5 +1,5 @@
-/*Getting data for rentals*/
-function loadrentdata() {
+/*Getting data for rentals/table?*/
+/*function loadrentdata() {
 const requestURL = 'https://raw.githubusercontent.com/techmike12/techmike12.github.io/master/Scoots/data/rentdata.json';
 
 fetch(requestURL)
@@ -36,7 +36,7 @@ fetch(requestURL)
             document.querySelector('div.rentals').appendChild(card);
     }
   });
-}
+}*/
 
 /*Display date in the footer*/
 document.getElementById("modified").innerHTML = "Last updated: " + document.lastModified;
